928444b371db32209d92163545729a81dabe4199
Тюрин Матвей <113491580+lllye@users.noreply.github.com>
2023-04-19 23:11:19 +0700

    Add files via upload

---
+++ Func/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Func
+{
+    public class Class1
+    {
+       public static void SumMult(double[] arr)
+        {
+            double sum = 0;
+            double mul = 1;
+            foreach (double n in arr)
+            {
+                sum += n;
+                mul *= n;
+            }
+            Console.WriteLine("Сумма чисел равна {0}\nПроизведение чисел равно {1}", sum, mul);
+
+        }
+    }
+}
